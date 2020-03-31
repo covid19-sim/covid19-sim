610632e602835843c3b1600f63fa94d5e5978c77
Sudesh Sahu <sahu.k.sudesh@gmail.com>
2020-03-31 05:47:02 -0500

    Refactored code, merged in code from Jatin's branch. Increased wall thickness but tunneling still occuring. Made the force vector use unit vectors for the direction instead of just the raw displacement vector but now things are laggy.

--- src/app.js
+++ src/app.js
@@ -7,9 +7,11 @@ var Engine = Matter.Engine,
 
 //constants
 const RADIUS = 5;
-const WIDTH = 800;
-const HEIGHT = 800;
-const TIME_DUR = 10 * 1000;
+const BALL_COUNT = 10;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+const WALL_THICKNESS = 20;
+const VEL_MULT = 6;
 
 // create an engine
 const engine = Engine.create();
@@ -20,68 +22,123 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-      height: HEIGHT,
-      width: WIDTH,
+      height: CANVAS_HEIGHT,
+      width: CANVAS_WIDTH,
       background: 'transparent',
     },
 });
 
-// Create a Runner
-const runner = Runner.create();
-
 // Gets random integers within a certain range
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function getRandomArbitrary(min, max) {
+
+const getRandomArbitrary = (min, max) => {
   return Math.random() * (max - min) + min;
 }
 
-
-var bodies = []
 // Create Walls
-let wallOptions = {inertia: Infinity, restitution: 1, isStatic: true, friction: 0, frictionAir: 0, frictionStatic: 0};
-topWall = Bodies.rectangle(400,1, 800,1, wallOptions);
-bottomWall = Bodies.rectangle(400,799, 800,1, wallOptions);
-leftWall = Bodies.rectangle(1, 400, 1, 800, wallOptions);
-rightWall = Bodies.rectangle(799, 400, 1, 800, wallOptions);
-bodies.push(topWall, bottomWall, leftWall, rightWall);
+const createWalls = () => {
+  let wallOptions = {
+    inertia: Infinity, 
+    restitution: 1, 
+    isStatic: true, 
+    friction: 0, 
+    frictionAir: 0, 
+    frictionStatic: 0
+  };
+  topWall = Bodies.rectangle(CANVAS_WIDTH/2, WALL_THICKNESS/2, CANVAS_WIDTH, WALL_THICKNESS, wallOptions);
+  bottomWall = Bodies.rectangle(CANVAS_WIDTH/2, CANVAS_HEIGHT- WALL_THICKNESS/2, CANVAS_WIDTH, WALL_THICKNESS, wallOptions);
+  leftWall = Bodies.rectangle(WALL_THICKNESS/2, CANVAS_HEIGHT/2, WALL_THICKNESS, CANVAS_HEIGHT, wallOptions);
+  rightWall = Bodies.rectangle(CANVAS_WIDTH- WALL_THICKNESS/2, CANVAS_HEIGHT/2, WALL_THICKNESS, CANVAS_HEIGHT, wallOptions);
+  return [topWall, bottomWall, leftWall, rightWall];
+}
 
-// Create circles
-for (var i = 0; i < 2000; i++) {
-  const x = getRandomIntInclusive(RADIUS, WIDTH-RADIUS);
-  const y = getRandomIntInclusive(RADIUS, HEIGHT-RADIUS);
-  const circleOptions = {inertia: Infinity, restitution: 1, friction: 0, frictionAir: 0, frictionStatic: 0};
-  const circle = Bodies.circle(x, y, RADIUS, circleOptions);
-  const vx = 5 * getRandomArbitrary(-1, 1);
-  const vy = 5 * getRandomArbitrary(-1, 1); 
-  const velocity = {x: vx, y: vy};
-  Matter.Body.setVelocity(circle, velocity);
-  bodies.push(circle);
+const createBalls = () => {
+  let balls = [];
+  for (var i = 0; i < BALL_COUNT; i++) {
+    const x = getRandomIntInclusive(RADIUS+WALL_THICKNESS, CANVAS_WIDTH-WALL_THICKNESS-RADIUS);
+    const y = getRandomIntInclusive(RADIUS+WALL_THICKNESS, CANVAS_HEIGHT-WALL_THICKNESS-RADIUS);
+    const ballOptions = {
+            inertia: Infinity, 
+            restitution: 1, 
+            friction: 0, 
+            frictionAir: 0, 
+            frictionStatic: 0
+          };
+    const ball = Bodies.circle(x, y, RADIUS, ballOptions);
+    const vx = VEL_MULT * getRandomArbitrary(-1, 1);
+    const vy = VEL_MULT * getRandomArbitrary(-1, 1); 
+    const velocity = {x: vx, y: vy};
+    Matter.Body.setVelocity(ball, velocity);
+    balls.push(ball);
+  }
+  return balls;
+}
+
+// Gets distance between two (x,y) points
+const distance_f = (a,b) => {
+  return Math.sqrt(Math.pow(a.x - b.x, 2) +  Math.pow(a.y - b.y, 2));
+}
+
+const compute_fv = (b1, b2) => {
+	const dist = Math.sqrt((b1['x'] - b2['x'])**2 + (b1['y'] - b2['y'])**2);
+	if (dist === 0) {
+		return Matter.Vector.create(0, 0);
+	}
+  let vdir = Matter.Vector.create(b1['x'] - b2['x'], b1['y'] - b2['y']); // points away from b2
+  // const constant = 50/10000; // 5 pixels = 0.001 in the opposite direction
+  vdir = Matter.Vector.normalize(vdir); // converts to a unit vector
+  const constant = 100;
+	return Matter.Vector.mult(vdir, constant/(dist**2));
+}
+
+const update_tree = () => {
+	for (let i = 0; i < balls.length; i++) {
+		TREE.insert(balls[i].position);
+	}
+}
+
+const clean_tree = () => {
+	for (let i = 0; i < balls.length; i++) {
+		TREE.remove(balls[i].position);
+	}
+}
+
+const update = () => {
+  update_tree();
+	for (let i = 0; i < balls.length; i++) {
+		const body = balls[i];
+		const b = body.position
+		const matches = TREE.nearest(b, 10, 100);
+		if (matches.length <= 1) {
+			// there can be one match, the ball itself
+			continue;
+		}
+		let fv = compute_fv(b, matches[0][0]);
+		for (let j = 1; j < matches.length; j++) {
+			fv = Matter.Vector.add(fv, compute_fv(b, matches[j][0]));
+		}
+		Matter.Body.applyForce(body, b, fv);
+	}
+	clean_tree();
 }
 
 // add all of the bodies to the world
-World.add(engine.world, bodies);
+const walls = createWalls();
+const balls = createBalls();
+World.add(engine.world, walls);
+World.add(engine.world, balls);
 
-// run the engine
-Engine.run(engine);
+// setup KDTree
+let TREE = new kdTree([], distance_f, ["x", "y"]);
 
-// run the renderer
-Render.run(render);
+Matter.Events.on(engine, "beforeUpdate", update);
 
-// let start = null;
-
-// function run(timestamp) {
-//   if (!start) start = timestamp;
-//   var progress = timestamp - start;
-//   Engine.update(engine, 1000/60);
-//   console.log("hi");
-  
-//   if (progress < TIME_DUR) {
-//     window.requestAnimationFrame(run);
-//   }
-// }
-
-// window.requestAnimationFrame(run);
+window.addEventListener('load', function() {
+  //Start the engine
+  Engine.run(engine);
+  Render.run(render);
+});
